68499632d31c9d138803a4f30f8b806620d40443
ginv <ginvinat@gmail.com>
2025-04-17 17:47:52 +0800

    Update pages.js

--- assets/js/pages.js
+++ assets/js/pages.js
@@ -3,7 +3,7 @@ const htmlPages = [
   "articles/亚马逊佣金分类目录.html",
   "calculators/亚马逊促销优惠券费用测算器.html",
   "calculators/bmi.html",
-  "亚马逊标品&非标品运营指南.html",
-  "新品计划指南.html"
+  "articles/亚马逊标品&非标品运营指南.html",
+  "articles/新品推广计划.html"
   // 后续新增页面在此手动添加即可
 ];
